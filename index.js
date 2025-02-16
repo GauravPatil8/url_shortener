32a08d9a5d3f5048ef4d4a4fd73d9923c9500d62
GauravPatil8 <patilgauravpradeep@gmail.com>
2025-02-16 15:51:40 +0530

    Added POST api, return shortid

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const connectToMongo = require("./connect");
+const urlRoute = require("./routes/url");
+
+
+const app = express();
+const PORT = 8000;
+
+connectToMongo("mongodb://127.0.0.1:27017/urlShortener").then(console.log("mongoDB connected"));
+app.use(express.json());
+app.use("/url", urlRoute); 
+
+app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}/`));
